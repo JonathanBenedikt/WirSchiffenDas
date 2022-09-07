cf3251706628a2c79a1096396292a24ddc4765e4
ViperPeak <nic.w.96@googlemail.com>
2022-09-07 18:09:12 +0200

    Die Docker-Compose wurde angepasst, damit alle Microservices zusammen in Docker bereitgestellt werden können.

--- Workflow_Engine_MS/src/main/java/com/Workflow_Engine/KafkaProducerConfig.java
+++ Workflow_Engine_MS/src/main/java/com/Workflow_Engine/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Map> jsonProducer(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.4.2:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
